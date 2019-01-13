5d9d937f3b3b44936e645d4fb2898356295b296c
tumanjongbt <tumanjongbt@users.noreply.github.com>
2019-01-13 01:59:50 -0500

    updates to views

--- BandSite/apps/bandsite_app/views.py
+++ BandSite/apps/bandsite_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 def index(request):
@@ -7,15 +9,12 @@ def index(request):
 def merch(request):
   return render(request, 'bandsite_app/merch.html')
 
-def checkout(request):
-  print(request.form)
-  merch_count = int(request.POST['record']) + int(request.POST['tshirt']) + int(request.POST['hoodie'])
-  print(merch_count)
-  return render(request, 'bandsite_app/merch.html', merch_count=merch_count)
-
-
 def tour(request):
   return render(request, 'bandsite_app/tour.html')
 
 def listen(request):
   return render(request, 'bandsite_app/listen.html')
+
+def register(request):
+  form = UserCreationForm()
+  return render(request, 'bandsite_app/register.html', {'form': form})
