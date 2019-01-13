d145d4da750116efd3534c0704215ee8980a684b
Aleksandra Dooley <sashadooley@gmail.com>
2019-01-13 00:09:59 -0700

    Unfinished checkout.

--- BandSite/apps/bandsite_app/views.py
+++ BandSite/apps/bandsite_app/views.py
@@ -11,6 +11,19 @@ def checkout(request):
   print(request.form)
   merch_count = int(request.POST['record']) + int(request.POST['tshirt']) + int(request.POST['hoodie'])
   print(merch_count)
+  # Set your secret key: remember to change this to your live secret key in production
+  # See your keys here: https://dashboard.stripe.com/account/apikeys
+  stripe.api_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
+  # Token is created using Checkout or Elements!
+  # Get the payment token ID submitted by the form:
+  token = request.form['stripeToken'] # Using Flask
+  charge = stripe.Charge.create(
+      amount=999,
+      currency='usd',
+      description='Example charge',
+      source=token,
+      metadata={'order_id': 6735},
+  )
   return render(request, 'bandsite_app/merch.html', merch_count=merch_count)
 
 
