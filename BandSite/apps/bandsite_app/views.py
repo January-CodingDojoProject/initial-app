5b95664d2ef951edfbbf5b8ca54a9186ced57d15
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-01-13 07:56:59 -0800

    added login functionality

--- BandSite/apps/bandsite_app/views.py
+++ BandSite/apps/bandsite_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import User, Tour, Product
+from django.contrib import messages
+import bcrypt
 
 # Create your views here.
 def index(request):
@@ -23,13 +25,20 @@ def checkout(request):
   pass
 
 def admin(request):
-  pass
+  return render(request, 'bandsite_app/admin.html')
 
 def login(request):
-  pass
+  users = User.objects.filter(email=request.POST["email"])#retrieve record in database to see if email exists
+  if len(users) > 0: #if I found a user
+    user = users[0]
+    if bcrypt.checkpw(request.POST['password'].encode(), user.pw_hash.encode()):   #checking to make sure the password is correct
+      request.session['id'] = user.id 
+      return redirect('/dashboard')
+  messages.error(request, 'invalid credentials')#if either of the above if's fail
+  return redirect('/admin')
 
 def dashboard(request):
-  pass
+  return render(request, 'bandsite_app/dashboard.html')
 
 def created(request):
   pass
